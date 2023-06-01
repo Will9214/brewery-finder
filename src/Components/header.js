ead62d6edcbe2b53fd3dc377c31d92cc37237214
Peter-JC-Wu <peterjcwu@gmail.com>
2023-06-01 11:40:37 -0400

    update header

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,8 +1,8 @@
 const Header = (props) => {
   return (
     <div className="container text-center">
-      <div className="container">
-        <h1 className="display-1 text-center">Brewery Finder</h1>
+      <div className="row justify-content-md-center">
+        <h1 className="col-lg-9 display-1 text-center">Brewery Finder</h1>
       </div>
       <div className="container">
         {props.children}
