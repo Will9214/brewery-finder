54dc7b1c954bfb30e7e9ecf87ca5b404a23906d5
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-06-02 11:38:21 -0400

    worked on beer rating slider

--- src/Reducers/reducer-entries.js
+++ src/Reducers/reducer-entries.js
@@ -3,7 +3,6 @@ import { FETCH_BREWERY } from "../Actions";
 const DEFAULT_STATE = {};
 
 const breweryReducer = function (state = DEFAULT_STATE, action) {
-  debugger;
   switch (action.type) {
     case FETCH_BREWERY:
       return {
