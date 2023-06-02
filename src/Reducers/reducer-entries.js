f4e0af58ded0888ab207798a23f37c09241e3e34
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-06-02 15:49:02 -0400

    added renderBreweryList function in brewery-results component to show a list of results after search

--- src/Reducers/reducer-entries.js
+++ src/Reducers/reducer-entries.js
@@ -3,10 +3,11 @@ import { FETCH_BREWERY } from "../Actions";
 const DEFAULT_STATE = {};
 
 const breweryReducer = function (state = DEFAULT_STATE, action) {
+  
   switch (action.type) {
     case FETCH_BREWERY:
       return {
-
+        breweries: action.payload.data
       }
     default:
       return state;
