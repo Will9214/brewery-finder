d3a11533038859c90dd6f32d08b97706e6008895
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-06-05 18:47:57 -0400

    refactored favorites to be an array of objects in state and updated the corresponding code in favorites-list and favorite-item component

--- src/Components/favorites-list.js
+++ src/Components/favorites-list.js
@@ -12,19 +12,17 @@ const FavoritesList = () => {
   const favorites = useSelector(state => state.favorites);
   
   const renderFavoritesList = () => {
+
     if (favorites) {
-      let favList = Object.entries(favorites).map((e) => ({[e[0]]: e[1]}));
-      return favList.map((favorite) => {
-        for (let prop in favorite) {
-          return (
-            <div className="fav-list col-6 col-lg-12 list-item" 
-              key={favorite[prop].info.id} 
-              id={favorite[prop].info.id} 
-              onClick={handleRowClick}>
-              {favorite[prop].info.name}
-            </div>
-          )
-        }
+      return favorites.map((favorite) => {
+        return (
+          <div className="fav-list col-6 col-lg-12 list-item" 
+            key={favorite.id} 
+            id={favorite.id} 
+            onClick={handleRowClick}>
+            {favorite.name}
+          </div>
+        )
       })
     }
   }
